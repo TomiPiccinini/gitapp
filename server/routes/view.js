4692edc78ffaeabee98487634262d3265383f934
Leof098 <70485779+Leof098@users.noreply.github.com>
2021-05-05 19:27:54 -0300

    Agrega vista de egresos

--- server/routes/view.js
+++ server/routes/view.js
@@ -10,4 +10,9 @@ router.get('/income', function (req, res) {
     res.render('income');
 });
 
+router.get('/egress', function (req, res) {
+    res.render('egress');
+});
+
+
 module.exports = router;
