86eb6c19f21dd8b7ae08e30924ddd7d502f640b9
LucasPerrino <chucho.lp@hotmail.com>
2021-06-17 21:18:36 -0300

    fix de que no se visualizaban los puntos

--- client/js/utils.js
+++ client/js/utils.js
@@ -1,32 +1,9 @@
 export function monefy(num) {
     if (!num) return '';
-
-    /*const numStr = String(num);
-    const points = numStr.length / 3;
-    const result = [];
-
-    for (let i = 0; i < points; i++) {
-        const s = -3 * (i + 1);
-        const e = -3 * i || undefined;
-        const chunk = numStr.slice(s, e);
-
-        result.push(chunk);
-        result.reverse().join('.');
-    }*/
-
-    const parteEntera = Math.trunc(num);
-    const parteDecimal = (num + "").split(".")[1];
-
-    let resultado = String(parteEntera)+","+String(parteDecimal);
-    if(!parteDecimal){
-        return parteEntera;
-    }
-    else{
-        return resultado;
-    }
-
-
-
+   
+    var partes = num.toString().split(".");
+    partes[0]=partes[0].replace(/\B(?=(\d{3})+(?!\d))/g,".");
+    return partes.join(",");
     
 }
 
