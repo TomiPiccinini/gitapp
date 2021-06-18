0d9a4115a3da6bfacb8773efad08e0da9c84ebfc
Tomas Piccinini <tomypiccinini1@gmail.com>
2021-06-18 15:13:29 -0300

    agrego test para resolver bug al crear fecha

--- tests/e2e/specs/income.spec.js
+++ tests/e2e/specs/income.spec.js
@@ -62,4 +62,18 @@ describe('Ingresos Test', () => {
 
         cy.get(':nth-child(5) > [data-testid=movement] > .level-right > :nth-child(1) > .has-text-success').should('include.text', '1.234,56')
     });
+
+    it('Deberia poder crear un nuevo ingreso con fecha distinta a la de hoy', () => {
+        cy.visit('/income');
+        cy.get('input[name=date]').type('2021-02-02');
+        cy.get('input[name=category]').type('Bono');
+        cy.get('input[name=amount]').type('100000');
+        cy.get('input[name=descripcion]').type('descripcion');
+        cy.contains('Guardar').click();
+        cy.get('body > main > div > div > div:nth-child(2) > div > div.card-content > div > ul > li:nth-child(5) > div > div.level-left > div:nth-child(2) > div > p.has-text-weight-light.is-size-7').should(($date) => {
+            
+            //Pongo un día menos porque creo que el horario del servidor cambia con el local y resta 1 día, pero deja crear el movimiento con un dia distinto al de hoy, que es lo que estoy resolviendo.
+            expect($date.text().trim()).equal('1/2/2021');
+          });
+    });
 });
